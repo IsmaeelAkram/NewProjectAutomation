587e6ef3b064d04a2300d87138a20f174e0e94a7
Ismaeel <ismaeelakram01@gmail.com>
2021-03-25 22:40:28 -0400

    Add remove command

--- commands.sh
+++ commands.sh
@@ -1,11 +1,10 @@
 function create(){
-    DIRECTORY=/Users/ismaeel/Documents/Code
-
     if [ $# -eq 0 ]
     then
         echo "No arguments supplied"
         return
     fi
+    DIRECTORY=/Users/ismaeel/Documents/Code
     cd $DIRECTORY
     gh repo create $1 -y --public
     cd $1
@@ -15,4 +14,18 @@ function create(){
     git commit -m "Initial commit"
     git push -u origin master
     code .
+}
+
+function remove(){
+    if [ $# -eq 0 ]
+    then
+        echo "No arguments supplied"
+        return
+    fi
+    DIRECTORY=/Users/ismaeel/Documents/Code
+    GH_USERNAME="IsmaeelAkram"
+    cd $DIRECTORY
+    sudo rm -rf $1
+    echo "Removed local directory. You're gonna have to delete the repo yourself."
+    echo "https://github.com/$GH_USERNAME/$1"
 }
