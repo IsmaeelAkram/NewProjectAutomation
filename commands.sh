53ecce4dd85950e29aa6e09c2575790ead5f729b
Ismaeel <19865145+IsmaeelAkram@users.noreply.github.com>
2021-07-25 22:37:20 -0400

    Add support for go projects

--- commands.sh
+++ commands.sh
@@ -38,6 +38,62 @@ function create(){
     code .
 }
 
+function creatego(){
+    if [ $# -eq 0 ]
+    then
+        echo "No arguments supplied"
+        return
+    fi
+    DIRECTORY=/Users/ismaeel/go/src/github.com/IsmaeelAkram
+    GIT_IGNORE="*.env"
+
+    cd "$DIRECTORY"
+
+    if [ "$2" = "--private" ]
+    then
+        gh repo create "$1" -y --private
+        cd "$1"
+        LOCAL=0
+    elif [ "$2" = "--local" ]
+    then
+        echo Not making a Github repo
+        mkdir "$1"
+        cd "$1"
+        git init .
+        LOCAL=1
+    else
+        gh repo create "$1" -y --public
+        cd "$1"
+        LOCAL=0
+    fi
+    touch README.md
+    echo \# "$1" >> README.md 
+    echo "$GIT_IGNORE" >> .gitignore
+    go mod init
+    git add .
+    git commit -m "Initial commit"
+    if [ LOCAL = 1 ]
+    then
+        git push -u origin master
+    fi
+    code .
+}
+
+function removego(){
+    if [ $# -eq 0 ]
+    then
+        echo "No arguments supplied"
+        return
+    fi
+    DIRECTORY=/Users/ismaeel/go/src/github.com/IsmaeelAkram
+    GH_USERNAME="IsmaeelAkram"
+    
+    cd $DIRECTORY
+    sudo rm -rf "$1"
+    echo "Removed local directory. You're gonna have to delete the repo yourself."
+    echo "https://github.com/$GH_USERNAME/$1/settings"
+}
+
 function remove(){
     if [ $# -eq 0 ]
     then
